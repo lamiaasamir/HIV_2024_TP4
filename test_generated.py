b70516f60d189a054b16835c3defc00323c4a566
lamiaasamir <lamiiaasamiir@gmail.com>
2024-03-30 14:34:25 -0400

    update 1

--- test_generated.py
+++ test_generated.py
@@ -152,5 +152,3 @@ def test_file_name_check(file_name_check):
     assert file_name_check("example.txt") == 'Yes'
     assert file_name_check("1example.dll") == 'No'
     assert file_name_check('.txt') == 'No'
-
- 
