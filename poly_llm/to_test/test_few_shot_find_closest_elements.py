b71ceaa3ed9571731bb5f354752b762361977ed9
lamiaasamir <lamiiaasamiir@gmail.com>
2024-04-02 16:59:29 -0400

    minor edit

--- poly_llm/to_test/test_few_shot_find_closest_elements.py
+++ poly_llm/to_test/test_few_shot_find_closest_elements.py
@@ -3,6 +3,6 @@ from find_closest_elements import find_closest_elements
 import unittest
 class TestClosestElements(unittest.TestCase):
     def test_find_closest_elements(self):
-        result = find_closest_elements([-6,-7,8,2])
-        expectedResult = (-7,-6)
+        result = find_closest_elements([6,7,8,2])
+        expectedResult = (6,7)
         self.assertEqual(result, expectedResult)
